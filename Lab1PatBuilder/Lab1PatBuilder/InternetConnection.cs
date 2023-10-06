9b9a35aca94ca70f08156ed517ffee0a39ac0f68
VeronikaAndrusyk <127151583+VeronikaAndrusyk@users.noreply.github.com>
2023-10-06 15:43:04 +0300

    Add files via upload

---
+++ Lab1PatBuilder/Lab1PatBuilder/InternetConnection.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class InternetConnection
+{
+    public string Protocol { get; private set; }
+    public string Encryption { get; private set; }
+    public string IpAddressVersion { get; private set; }
+    public Dictionary<string, string> OtherProperties { get; private set; }
+
+    private InternetConnection() { }// приватний конструктор
+
+    // внутрішній клас для побудови з'єднання з Інтернетом
+    public class InternetConnectionBuilder
+    {
+        private InternetConnection connection;
+
+        public InternetConnectionBuilder()
+        {
+            connection = new InternetConnection();
+        }
+
+        public InternetConnectionBuilder WithProtocol(string protocol)//цей метод приймає рядок protocol і встановлює властивість Protocol
+                                                                      //об'єкта InternetConnection на передане значення protocol
+        {
+            connection.Protocol = protocol;
+            return this;
+        }
+
+        public InternetConnectionBuilder WithEncryption(string encryption)
+        {
+            connection.Encryption = encryption;
+            return this;
+        }
+
+        public InternetConnectionBuilder WithIpAddressVersion(string ipAddressVersion)
+        {
+            connection.IpAddressVersion = ipAddressVersion;
+            return this;
+        }
+
+        public InternetConnectionBuilder WithCustomProperty(string key, string value)//цей метод приймає два рядки: key і value. Він використовує
+                                                                                     //OtherProperties, як словник (Dictionary), щоб зберігати додаткові
+                                                                                     //користувацькі властивості об'єкта InternetConnection
+        {
+            if (connection.OtherProperties == null)
+            {
+                connection.OtherProperties = new Dictionary<string, string>();
+            }
+
+            connection.OtherProperties[key] = value;
+            return this;
+        }
+
+        public InternetConnection Build()
+        {
+            return connection;
+        }
+    }
+
+    public void Connect()
+    {
+        Console.WriteLine($"Connecting using {Protocol} protocol...");
+
+        if (!string.IsNullOrEmpty(IpAddressVersion))
+        {
+            Console.WriteLine($"Using {IpAddressVersion} IP address version...");
+        }
+        else
+        {
+            Console.WriteLine("No IP address version specified. Using default (IPv4)...");
+        }
+
+        if (!string.IsNullOrEmpty(Encryption))
+        {
+            Console.WriteLine($"Using {Encryption} encryption...");
+        }
+        else
+        {
+            Console.WriteLine("No encryption specified.");
+        }
+
+        if (OtherProperties != null)
+        {
+            foreach (var property in OtherProperties)
+            {
+                Console.WriteLine($"Custom property: {property.Key} = {property.Value}");
+            }
+        }
+    }
+}
---
+++ Lab1PatBuilder/Lab1PatBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Використання білдера для створення з'єднань з Інтернетом
+
+        InternetConnection connection1 = new InternetConnection.InternetConnectionBuilder()
+            .WithProtocol("HTTP")
+            .WithCustomProperty("CustomProperty1", "Value1")
+            .WithCustomProperty("CustomProperty2", "Value2")
+            .Build();
+
+        InternetConnection connection2 = new InternetConnection.InternetConnectionBuilder()
+            .WithProtocol("HTTPS")
+            .WithEncryption("TLS")
+            .WithIpAddressVersion("IPv6")
+            .WithCustomProperty("CustomProperty3", "Value3")
+            .WithCustomProperty("CustomProperty4", "Value4")
+            .Build();
+
+        Console.WriteLine("Connection 1:");
+        connection1.Connect();
+
+        Console.WriteLine("\nConnection 2:");
+        connection2.Connect();
+    }
+}
+
